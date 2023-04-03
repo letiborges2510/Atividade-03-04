b0501ec9f967711b52151975cdc38ec2a63d3c3f
Letícia Borges <99838964+letiborges2510@users.noreply.github.com>
2023-04-03 14:33:50 -0300

    Update index.php

--- Atividade-03-04/index.php
+++ Atividade-03-04/index.php
@@ -17,7 +17,7 @@
           <div class="collapse navbar-collapse" id="navbarNavDropdown">
             <ul class="navbar-nav">
               <li class="nav-item">
-                <a class="nav-link active" aria-current="page" href="#">Cadastrar</a>
+                <a class="nav-link active" aria-current="page" href="index.php">Cadastrar</a>
               </li>
               <li class="nav-item">
                 <a class="nav-link" href="consulta.php">Consultar</a>
